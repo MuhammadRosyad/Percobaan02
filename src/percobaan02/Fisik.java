2d4aea219f9b295a068afd58155e8ed4e3177173
MuhammadRosyad <achaibuy01@gmail.com>
2018-04-01 20:57:35 +0800

    Membuat Atribut dan Behavior

---
+++ src/percobaan02/Fisik.java
@@ -0,0 +1,12 @@
+package percobaan02;
+public class Fisik {
+    double berat;
+    double tinggi;
+    
+    void FisikInfo(){
+        System.out.printIn("==================");
+        System.out.printIn("Berat :"+berat);
+        System.out.printIn("Tinggi :"+tinggi);
+        System.out.printIn("==================");
+    }
+}
