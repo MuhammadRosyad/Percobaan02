f50a0302dd72686ea4b47b8fa9b8a81a0213d534
MuhammadRosyad <achaibuy01@gmail.com>
2018-04-01 21:05:43 +0800

    Mengisi Nilai Atribut

--- src/percobaan02/FisikAksi.java
+++ src/percobaan02/FisikAksi.java
@@ -3,5 +3,7 @@ package percobaan02;
 public class FisikAksi {
     public static void main(String[] args) {
         Fisik r1 = new Fisik();
+        r1.berat = 50;
+        r1.tinggi = 175;
     }
 }
